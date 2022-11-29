000bb038ba3db46c97b54455c7bd601e078194c1
sercandia21 <a21sercandia@inspedralbes.cat>
2022-11-29 13:59:50 +0100

    Controllers y Models hechos

--- web/leagueOfTrivialG2/app/Models/User.php
+++ web/leagueOfTrivialG2/app/Models/User.php
@@ -12,6 +12,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function challenge()
+    {
+        return $this->belongsTo(Challenge::class);
+    }
+    public function ranking()
+    {
+        return $this->belongsToMany(Ranking::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
