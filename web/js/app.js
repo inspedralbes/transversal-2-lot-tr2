d6a36ac1c2ccbbbf909bf417981097c69393b8a6
sercandia21 <a21sercandia@inspedralbes.cat>
2022-12-03 19:35:33 +0100

    Componente register añadido (base)

--- web/js/app.js
+++ web/js/app.js
@@ -329,7 +329,9 @@ const login = Vue.component("login", {
                             <h3 class="app__titol">Login</h3>
                             <b-form-input id="input-2" v-model="form.username" placeholder="Write your username..." required></b-form-input><br>
                             <b-form-input id="input-2" v-model="form.password" type="password" placeholder="AÑADIR CONTRASEÑA LARAVEL..." required></b-form-input><br>
-                            Don't have an account yet?<a href="/register">Join the league now!</a> <br>
+                            Don't have an account yet?<router-link to="/register" style="text-decoration: none;">
+                            <h6 style="display: inline;">Join the league now!</h6>
+                        </router-link> <br>
                         </div>
                             <template #modal-footer>
                                 <div v-show="!isLogged">
@@ -397,6 +399,101 @@ const login = Vue.component("login", {
 
 });
 
+const register=Vue.component("register",{
+    data: function () {
+        return {
+            //Creamos objeto para tener la informacion del usuario junta.
+            processing: false,
+            form: {
+                name:'',
+                username: '',
+                email:'',
+                password: ''
+            },
+            show: true
+
+            
+        }
+    },
+    template:`<div>
+                    <b-form v-if="show">
+                        <b-form-group
+                            id="input-group-1"
+                            label="Email address:"
+                            label-for="input-1"
+                            description="We'll never share your email with anyone else.">
+                                <b-form-input
+                                    id="input-1"
+                                    v-model="form.email"
+                                    type="email"
+                                    placeholder="Write an email..."
+                                    required
+                                >
+                                </b-form-input>
+                        </b-form-group>
+
+                        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+                        <b-form-input
+                            id="input-2"
+                            v-model="form.name"
+                            placeholder="Enter a name..."
+                            required
+                        ></b-form-input>
+                        </b-form-group>
+
+                        <b-form-group id="input-group-3" label="Your user name:" label-for="input-3">
+                        <b-form-input
+                            id="input-3"
+                            v-model="form.username"
+                            placeholder="Enter a usernamename..."
+                            required
+                        ></b-form-input>
+                        </b-form-group>
+
+                        <b-form-group
+                        id="input-group-4"
+                        label="Password:"
+                        label-for="input-4"
+                        description="We'll never share your email with anyone else.">
+                            <b-form-input
+                                id="input-4"
+                                v-model="form.password"
+                                type="password"
+                                placeholder="Write a password..."
+                                required
+                            >
+                            </b-form-input>
+                    </b-form-group>
+
+                        <b-button @click="send" variant="primary">Register</b-button>
+                        <b-button @click="onReset" type="reset" variant="danger">Reset</b-button>
+                    </b-form>
+                    <b-card class="mt-3" header="Form Data Result">
+                        <pre class="m-0">{{ form }}</pre>
+                    </b-card>
+                </div>`,
+    methods:{
+        send:function(event){
+            event.preventDefault()
+            alert(JSON.stringify(this.form))
+        },
+        onReset:function(event) {
+            event.preventDefault()
+            // Reset our form values
+            this.form.email = ''
+            this.form.name = ''
+            this.form.userName = null
+            this.form.password = null
+            // Trick to reset/clear native browser form validation state
+            this.show = false
+            this.$nextTick(() => {
+                this.show = true
+            })
+           
+        }      
+    }
+})
+
 // -----------------Vue Routes-----------------
 const routes = [{
     path: '/game',
@@ -404,7 +501,11 @@ const routes = [{
 }, {
     path: '/login',
     component: login
-}, {
+}, 
+{
+    path: '/register',
+    component: register
+},{
     path: '/',
     component: home
 }]
