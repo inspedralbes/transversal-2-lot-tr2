fb611d4db21a308a5b7d929bc33d2887eeda58c4
sercandia21 <a21sercandia@inspedralbes.cat>
2022-12-05 10:01:40 +0100

    Kernel schedule daily quiz

--- web/leagueOfTrivialG2/app/Console/Kernel.php
+++ web/leagueOfTrivialG2/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +17,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $response = Http::get(`https://the-trivia-api.com/api/questions?limit=10`);
+
+            DB::table('games')->where('type', 'daily')->update(['quiz' => $response]);
+        })->dailyAt('10:01');
     }
 
     /**
@@ -25,7 +31,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
