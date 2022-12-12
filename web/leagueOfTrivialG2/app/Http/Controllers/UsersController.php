f9911073b23a0ca5233db61325d01d80db7f0ff9
sercandia21 <a21sercandia@inspedralbes.cat>
2022-12-12 13:28:42 +0100

    user historic method

--- web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
+++ web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
@@ -95,7 +95,7 @@ class UsersController extends Controller
     {
         $user = $request->idUser;
         $userInfo = DB::select('SELECT users.name, users.userName, users.email, users.status FROM users  WHERE users.id=' . $user . ';');
-        $historic = DB::select('SELECT rankings.puntuacio, rankings.idUser, rankings.idGame, games.date, games.difficulty, games.category FROM rankings JOIN games ON games.id=rankings.idGame where rankings.idUser=' . $user . ' ORDER BY games.date DESC;');
+        $historic = DB::select('SELECT rankings.puntuacio, rankings.idUser, rankings.idGame, games.date, games.difficulty, games.category FROM rankings JOIN games ON games.id=rankings.idGame where rankings.idUser=' . $user . ' && games.type="normal" ORDER BY games.date DESC;');
         $xp = DB::select('SELECT SUM(rankings.puntuacio) AS xp FROM rankings WHERE idUser=' . $user . ';');
         return response()->json(['info' => $userInfo, 'historic' => $historic, 'xp' => $xp]);
     }
