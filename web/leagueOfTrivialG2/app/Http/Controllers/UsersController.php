a6dd6aa4e1a2bdd3da8c1e6daeff30b5d37f952a
sercandia21 <a21sercandia@inspedralbes.cat>
2022-12-13 09:04:49 +0100

    Edit avatar implemented

--- web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
+++ web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
@@ -34,6 +34,7 @@ class UsersController extends Controller
             //Como se llaman los campos en BD
             $user->name = $request->name;
             $user->email = $request->email;
+            $user->imageUrl = $request->avatar;
             $user->password = Hash::make($request->password);
             $user->userName = $request->username;
 
@@ -94,7 +95,7 @@ class UsersController extends Controller
     public function userInfo(Request $request)
     {
         $user = $request->idUser;
-        $userInfo = DB::select('SELECT users.name, users.userName, users.email, users.status FROM users  WHERE users.id=' . $user . ';');
+        $userInfo = DB::select('SELECT users.name, users.userName, users.imageUrl, users.email, users.status FROM users  WHERE users.id=' . $user . ';');
         $historic = DB::select('SELECT rankings.puntuacio, rankings.idUser, rankings.idGame, games.date, games.difficulty, games.category FROM rankings JOIN games ON games.id=rankings.idGame where rankings.idUser=' . $user . ' && games.type="normal" ORDER BY games.date DESC;');
         $xp = DB::select('SELECT SUM(rankings.puntuacio) AS xp FROM rankings WHERE idUser=' . $user . ';');
         return response()->json(['info' => $userInfo, 'historic' => $historic, 'xp' => $xp]);
