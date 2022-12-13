22f1cdec20dfce49de92d4b6cb8c513f223d6e16
sercandia21 <a21sercandia@inspedralbes.cat>
2022-12-13 11:51:53 +0100

    select quant categories

--- web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
+++ web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
@@ -97,8 +97,9 @@ class UsersController extends Controller
         $user = $request->idUser;
         $userInfo = DB::select('SELECT users.name, users.userName, users.imageUrl, users.email, users.status FROM users  WHERE users.id=' . $user . ';');
         $historic = DB::select('SELECT rankings.puntuacio, rankings.idUser, rankings.idGame, games.date, games.difficulty, games.category FROM rankings JOIN games ON games.id=rankings.idGame where rankings.idUser=' . $user . ' && games.type="normal" ORDER BY games.date DESC;');
+        $quantCategories = DB::select('SELECT COUNT(rankings.idGame) AS "quant", games.category FROM rankings JOIN users ON users.id=rankings.idUser JOIN games ON games.id=rankings.idGame WHERE users.id=' . $user . ' GROUP BY games.category;');
         $xp = DB::select('SELECT SUM(rankings.puntuacio) AS xp FROM rankings WHERE idUser=' . $user . ';');
-        return response()->json(['info' => $userInfo, 'historic' => $historic, 'xp' => $xp]);
+        return response()->json(['info' => $userInfo, 'historic' => $historic, 'quantCateg' => $quantCategories, 'xp' => $xp]);
     }
     public function updateProfilePic(Request $request)
     {
