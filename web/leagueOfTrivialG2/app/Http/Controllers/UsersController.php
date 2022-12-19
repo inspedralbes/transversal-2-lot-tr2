c32da8be5df2d0ada8f22dbc1d61132de807ddc7
yolimoreno <a21yolmorarr@inspedralbes.cat>
2022-12-19 14:14:35 +0100

    mostra fa quants dies al ranking

--- web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
+++ web/leagueOfTrivialG2/app/Http/Controllers/UsersController.php
@@ -103,12 +103,11 @@ class UsersController extends Controller
         $userInfo = DB::select('SELECT users.name, users.userName, users.imageUrl, users.email, users.status, users.rupees FROM users  WHERE users.id=' . $user . ';');
         $historic = DB::select('SELECT rankings.puntuacio, rankings.idUser, rankings.idGame, games.date, games.difficulty, games.category FROM rankings JOIN games ON games.id=rankings.idGame where rankings.idUser=' . $user . ' && games.type="normal" ORDER BY rankings.created_at DESC;');
         $quantCategories = DB::select('SELECT COUNT(rankings.idGame) AS "quant", games.category FROM rankings JOIN users ON users.id=rankings.idUser JOIN games ON games.id=rankings.idGame WHERE users.id=' . $user . ' && games.type="normal" GROUP BY games.category;');
-        // $xp = DB::select('SELECT SUM(rankings.puntuacio) AS xp FROM rankings WHERE idUser=' . $user . ';');
         return response()->json(['info' => $userInfo, 'historic' => $historic, 'quantCateg' => $quantCategories]);
     }
     public function updateProfile(Request $request)
     {
-        DB::update('UPDATE users set imageUrl = "' . $request->imageUrl . '", name="'.$request->name.'", status="' . $request->status . '"  where id=' . $request->idUser . ';');
+        DB::update('UPDATE users set imageUrl = "' . $request->imageUrl . '", name="' . $request->name . '", status="' . $request->status . '"  where id=' . $request->idUser . ';');
         return response()->json(Response::HTTP_OK);
     }
     public function inGame(Request $request)
